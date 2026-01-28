d7ba5315ab435573c6ff580e2ba8fbf4814467ac
Joel Comberiati <joelcomberiati@MacBook-Pro.local>
2026-01-27 18:43:25 -0800

    Remove db.create_all; rely on Alembic migrations only

--- src/app.py
+++ src/app.py
@@ -175,8 +175,6 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    with app.app_context():
-        db.create_all()
 
     @app.route("/collect", methods=["POST"])
     def collect():
